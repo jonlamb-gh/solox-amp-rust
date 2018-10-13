0a3a14bbba0c460fff1546e4dcc0b3c9613ae407
jonlamb-gh <lamb.jon.io@gmail.com>
2018-10-13 06:04:39 -0700

    WIP MU

--- m4-firmware/rpmsg-lite/src/env.rs
+++ m4-firmware/rpmsg-lite/src/env.rs
@@ -1,3 +1,5 @@
+// TODO - figure out some traits after initial port
+
 #[derive(Copy, Clone, Debug)]
 pub struct Env {
     //
--- m4-firmware/rpmsg-lite/src/mu.rs
+++ m4-firmware/rpmsg-lite/src/mu.rs
@@ -5,12 +5,13 @@
 // TODO
 // - macro for generating A and B peripherals
 // - use an svd file
-// MU B 0x4229_C000
+// - inlining
+// - use HAL spirit of Errors (nb::WouldBlock/IsPending/etc)?
 
 use core::marker::PhantomData;
 
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
-pub enum RxRegister {
+pub enum MsgRegister {
     R0,
     R1,
     R2,
@@ -39,13 +40,13 @@ impl MU_B {
         );
     }
 
-    pub fn is_rx_full(&self, msg_register: RxRegister) -> bool {
+    pub fn is_rx_full(&self, msg_register: MsgRegister) -> bool {
         let rb = unsafe { &*Self::ptr() };
         rb.sr.register.get() & (MU_SR_RF0_MASK >> u32::from(msg_register)) != 0
     }
 
     // use nb::Error::WouldBlock?
-    pub fn try_recv_msg(&self, msg_register: RxRegister) -> Result<u32, ()> {
+    pub fn try_recv_msg(&self, msg_register: MsgRegister) -> Result<u32, ()> {
         if self.is_rx_full(msg_register) {
             let rb = unsafe { &*Self::ptr() };
             Ok(rb.rr(msg_register))
@@ -53,15 +54,50 @@ impl MU_B {
             Err(())
         }
     }
+
+    pub fn recv_msg(&self, msg_register: MsgRegister) -> u32 {
+        let rb = unsafe { &*Self::ptr() };
+        let mask = MU_SR_RF0_MASK >> u32::from(msg_register);
+
+        // wait for rx to be full
+        while (rb.sr.register.get() & mask) == 0 {}
+
+        rb.rr(msg_register)
+    }
+
+    pub fn is_general_int_accepted(&self, msg_register: MsgRegister) -> bool {
+        let rb = unsafe { &*Self::ptr() };
+        rb.sr.register.get() & (MU_CR_GIR0_MASK >> u32::from(msg_register)) == 0
+    }
+
+    /// Trigger a general purpose interrupt to the other core
+    /// Returns true if the interrupt was triggered
+    /// and false if the interrupt is currently pending
+    pub fn trigger_general_int(&self, msg_register: MsgRegister) -> bool {
+        if self.is_general_int_accepted(msg_register) {
+            // all interrupts have been accepted, trigger now
+            let rb = unsafe { &*Self::ptr() };
+            let cr = rb.cr.register.get();
+            rb.cr.register.set(
+                // clear GIRn
+                (cr & !MU_CR_GIRn_MASK)
+                // set GIRn
+                | (MU_CR_GIR0_MASK >> u32::from(msg_register))
+            );
+            true
+        } else {
+            false
+        }
+    }
 }
 
-impl From<RxRegister> for u32 {
-    fn from(r: RxRegister) -> u32 {
+impl From<MsgRegister> for u32 {
+    fn from(r: MsgRegister) -> u32 {
         match r {
-            RxRegister::R0 => 0,
-            RxRegister::R1 => 1,
-            RxRegister::R2 => 2,
-            RxRegister::R3 => 3,
+            MsgRegister::R0 => 0,
+            MsgRegister::R1 => 1,
+            MsgRegister::R2 => 2,
+            MsgRegister::R3 => 3,
         }
     }
 }
@@ -70,6 +106,7 @@ pub const NUM_RX_REGISTERS: u32 = 4;
 pub const NUM_TX_REGISTERS: u32 = 4;
 
 pub const MU_SR_RF0_MASK: u32 = 1 << 27;
+pub const MU_CR_GIR0_MASK: u32 = 1 << 19;
 
 pub const MU_CR_GIEn_MASK: u32 = 0xF000_0000;
 pub const MU_CR_RIEn_MASK: u32 = 0x0F00_0000;
@@ -92,12 +129,12 @@ struct RegisterBlock {
 }
 
 impl RegisterBlock {
-    fn rr(&self, msg_register: RxRegister) -> u32 {
+    fn rr(&self, msg_register: MsgRegister) -> u32 {
         match msg_register {
-            RxRegister::R0 => self.rr0.register.get(),
-            RxRegister::R1 => self.rr1.register.get(),
-            RxRegister::R2 => self.rr2.register.get(),
-            RxRegister::R3 => self.rr3.register.get(),
+            MsgRegister::R0 => self.rr0.register.get(),
+            MsgRegister::R1 => self.rr1.register.get(),
+            MsgRegister::R2 => self.rr2.register.get(),
+            MsgRegister::R3 => self.rr3.register.get(),
         }
     }
 }
