2f28f680855eb53187c2c47b796fa5e685648ccc
jonlamb-gh <lamb.jon.io@gmail.com>
2018-10-15 06:50:37 -0700

    WIP platform/env

--- m4-firmware/rpmsg-lite/src/platform.rs
+++ m4-firmware/rpmsg-lite/src/platform.rs
@@ -1,15 +1,34 @@
+//! WIP platfrom and bare metal environment layers merged into one
+
 // TODO
 // - turn this into generic traits and provide a default IMX6SX impl
 // - verify the need/behavior of the ISR/init/deinit counters used in the C impl
 // - MU_IRQ_PRIORITY
-// - can I seperate the interdependency of platform and environment layers calling into
-// each other?
+// - can I seperate the interdependency of platform and environment layers
+// calling into each other?
+// - vq_id type with methods to replace the C macros (ie RL_GET_Q_ID())
 
 use bare_metal::Nr;
 use cortex_m::peripheral::scb::VectActive;
 use cortex_m::{interrupt, peripheral};
 
 use mu;
+
+/// RPMSG MU channel index/register
+/// As Linux suggests, use MU channel 1 for as communication channel
+pub const RPMSG_MU_CHANNEL: mu::MsgRegister = mu::MsgRegister::R1;
+
+/// Linux requires the ALIGN to 0x1000(4KB) instead of 0x80
+pub const VRING_ALIGN: u32 = 0x1000;
+
+/// contains pool of descriptors and two circular buffers
+pub const VRING_SIZE: u32 = 0x8000;
+
+/// size of shared memory + 2*VRING size
+pub const VRING_OVERHEAD: u32 = (2 * VRING_SIZE);
+
+//#define RL_PLATFORM_IMX6SX_M4_LINK_ID (0)
+//#define RL_PLATFORM_HIGHEST_LINK_ID (0)
 
 pub const SYSTEM_CORE_CLOCK: u32 = 227_000_000;
 
@@ -38,6 +57,13 @@ unsafe impl Nr for Interrupt {
     }
 }
 
+// TODO - do something else with these, inline, etc
+//#define RL_GET_LINK_ID(id) (((id)&0xFFFFFFFE) >> 1)
+//#define RL_GET_Q_ID(id) ((id)&0x1)
+pub fn get_vq_id(core_id: u32, queue_id: u32) -> u32 {
+    (queue_id & 0x1) | ((core_id << 1) & 0xFFFFFFFE)
+}
+
 // TODO - figure out some traits after initial port
 impl<'a> Platform<'a> {
     pub fn new(nvic: &'a mut peripheral::NVIC) -> Self {
@@ -56,6 +82,18 @@ impl<'a> Platform<'a> {
         // NVIC_SetPriority(BOARD_MU_IRQ_NUM, APP_MU_IRQ_PRIORITY);
 
         self.nvic.enable(Interrupt::MU_M4);
+    }
+
+    pub fn notify(&self, vq_id: u32) {
+        // TODO
+    }
+
+    /// Called by application ISR
+    pub fn rpmsg_handler(&self) {
+        if let Ok(msg) = self.mu.recv_msg(RPMSG_MU_CHANNEL) {
+            // notification, channel is upper 16 bits
+            self.env_isr(msg >> 16)
+        }
     }
 
     pub fn time_delay(&self, ms: u32) {
@@ -89,4 +127,14 @@ impl<'a> Platform<'a> {
 
     // pub fn vatop()
     // pub fn patova()
+    //
+
+    //---------
+    // bm env stuff begins here
+    // --------
+
+    /// Called by application ISR
+    fn env_isr(&self, vector: u32) {
+        // TODO
+    }
 }
